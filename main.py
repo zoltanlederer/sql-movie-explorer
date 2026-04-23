6856eab11dc242ffea917114afd5971194a12468
Zoltan Lederer <23390588+zoltanlederer@users.noreply.github.com>
2026-04-23 20:04:53 +0200

    Add auto database setup to main.py

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ CLI tool to query a personal media library using SQLite.
 """
 
 import argparse
-from load_data import get_connection
+from pathlib import Path
+from load_data import load_csv, get_connection, import_data, db_exists
 from query import query
 
 if __name__ == '__main__':
+    # Set up CLI arguments
     parser = argparse.ArgumentParser(description='CLI tool to query a personal media library')
     parser.add_argument('-g', '--genres', help='Filter by genre, e.g. "Action", "Comedy"')
     parser.add_argument('-y', '--year', help='Filter by year, e.g. "1990"')
@@ -16,7 +18,17 @@ if __name__ == '__main__':
     parser.add_argument('-o', '--output', nargs='?', const='export.csv', help='Save results to CSV. Optionally provide a filename, e.g. "results.csv". Defaults to "export.csv" if no filename is given.')
     args = parser.parse_args()
 
-    connection = get_connection()
+    # Set up database
+    if not Path('./data/media.db').exists():
+        df = load_csv(Path('./data/master.csv'))
+        db_exists()
+        connection = get_connection()
+        import_data(df, connection)
+        print('Database created.')
+    else:
+        connection = get_connection()
+
+    # Run query and display results
     results = query(connection, genres=args.genres, year=args.year, imdb_rating=args.rating, media_type=args.media_type, title=args.title)
     
     if results.shape[0] > 0:
@@ -33,7 +45,8 @@ if __name__ == '__main__':
     else:
         print('No results found. Try different filters.')
 
-    if args.output: # save results to CSV
+    # Export results to CSV
+    if args.output:
         if results.shape[0] > 0:
             full_results = query(connection, genres=args.genres, year=args.year, imdb_rating=args.rating, media_type=args.media_type, title=args.title, full=True)
             full_results.drop(columns=['index'], inplace=True)
